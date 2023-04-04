9b325d9d93318e2e07d8a668796bba07d07d5019
Edwardd02 <122767943+Edwardd02@users.noreply.github.com>
2023-04-04 18:56:14 +0900

    where should insert bookFragment?

--- app/src/main/java/com/example/assignment07/BookPlayerFragment.kt
+++ app/src/main/java/com/example/assignment07/BookPlayerFragment.kt
@@ -5,16 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 
 class BookPlayerFragment : Fragment() {
     private lateinit var textViewModel: TextViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+        textViewModel = ViewModelProvider(requireActivity())[TextViewModel::class.java]
+        val bookFragment = BookFragment()
+        bookFragment.textViewModel = textViewModel
+        childFragmentManager.beginTransaction().add(R.id.bookFragment, bookFragment).commit()
     }
 
     override fun onCreateView(
